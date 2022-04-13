d2a4befdb7e3e9444c0650ff8c1c123bdda957dc
Hankom <4444pomen@naver.com>
2022-04-13 22:53:23 +0900

    initial

--- chainedmatrix.cpp
+++ chainedmatrix.cpp
@@ -1,38 +1,38 @@
-#include<iostream>
+#include <iostream>
  
 using namespace std;
  
-#define MIN(A, B) ((A)>(B)?(B):(A))
-#define MAX_VALUE 9999999
-#define MAX_SIZE 101
+#define minimum(A, B) ((A)>(B)?(B):(A))
+#define maximum_value 9999999
+#define maximum_size 101
  
-int M[MAX_SIZE][MAX_SIZE];
-int d[MAX_SIZE];
+int M[maximum_size][maximum_size];
+int d[maximum_size];
  
 int main()
 {
     int size = 4;
  
-    d[0] = 20, d[1] = 1, d[2] = 30, d[3] = 10, d[4] = 10;
+    d[0] = 10, d[1] = 1, d[2] = 40, d[3] = 30, d[4] = 10; //행렬 4개가 있을 때
  
-    for (int diagonal = 0; diagonal < size; diagonal++)
+    for (int diag = 0; diag < size; diag++)     //입력한 행렬의 사이즈만큼 반복한다.
     {
-        for (int i = 1; i <= size - diagonal; i++)
+        for (int i = 1; i <= size - diag; i++) // i값은 상단 1부터 시작하여, 반복하는 횟수를 1씩 감소한다
         {
-            int j = i + diagonal;
-            if (j == i)
+            int j = i + diag; //j값은 우측으로 diag만큼 반복할때마다 이동한다.
+            if (j == i) //i와 j가 같을 경우 M[i][j] = 0으로 정한다.
             {
                 M[i][j] = 0;
                 continue;
             }
-            M[i][j] = MAX_VALUE;
+            M[i][j] = maximum_value; //k=i~j-1만큼 반복하며, 식을 입력하여 M[i][j]에 들어갈 곱의 최소값을 구한다.
             for (int k = i; k <= j - 1; k++)
-                M[i][j] = MIN(M[i][j], M[i][k] + M[k + 1][j] + d[i - 1] * d[k] * d[j]);
+                M[i][j] = minimum(M[i][j], M[i][k] + M[k + 1][j] + d[i - 1] * d[k] * d[j]);
  
         }
     }
  
-    /*결과 출력*/
+    // 결과값 출력
     cout << M[1][size] << endl;
     for (int i = 1; i <= size; i++)
     {
